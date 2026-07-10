c7d88c424776056caa4c5a0c7d291153a208b9d9
Gurchetan Singh <gurchetan.singh2@icloud.com>
2026-07-10 11:25:35 +0530

    ADD:react basics

---
+++ introtoreact/basics/src/App.jsx
@@ -0,0 +1,22 @@
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import Welcome from "./Components/Welcome";
+import Home from "./navbar/Home";
+import Contact from "./navbar/Contact";
+import About from "./navbar/About";
+function App(){
+  return (
+    <BrowserRouter>
+    <nav style={{padding: "10px",background: "#eee"}}>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
---
+++ introtoreact/basics/src/navbar/Home.jsx
@@ -0,0 +1,9 @@
+function Home(){
+    return(
+        <div>
+            <h1>This is a homepage</h1>
+        </div>
+    )
+};
+
+export default Home;
